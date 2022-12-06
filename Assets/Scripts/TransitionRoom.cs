dec99f3f022df2029f20e55ffc23896141eb44fe
Falendrac <leger_franck@hotmail.fr>
2022-12-06 09:11:29 +0100

    Create blackscreen transition between scenes videos.

--- Assets/Scripts/TransitionRoom.cs
+++ Assets/Scripts/TransitionRoom.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEngine.UI;
 
 /// <summary>
 /// Able to transition of all rooms in the tour
@@ -16,6 +18,9 @@ public class TransitionRoom : MonoBehaviour
     GameObject LivingRoomCanvas;
     // Sphere where is the livingRoom video
     GameObject LivingRoomSphere;
+    // Canvas to make the blackscreen
+    GameObject BlackScreen;
+    Animator BlackScreenAnimator;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +29,23 @@ public class TransitionRoom : MonoBehaviour
         CantinaSphere = GameObject.Find("Cantina");
         LivingRoomCanvas = GameObject.Find("LivingRoomCanvas");
         LivingRoomSphere = GameObject.Find("LivingRoom");
+        BlackScreen = GameObject.Find("BlackScreen");
+        BlackScreenAnimator = BlackScreen.GetComponentInChildren<Animator>();
+    }
+
+    void ScreenFade()
+    {
+        BlackScreen.GetComponent<Canvas>().enabled = true;
+        BlackScreenAnimator.enabled = true;
+    }
+
+    async void ScreenUnfade()
+    {
+        BlackScreenAnimator.SetBool("TransitionFinished", true);
+        await Task.Delay(3500);
+        BlackScreenAnimator.SetBool("TransitionFinished", false);
+        BlackScreenAnimator.enabled = false;
+        BlackScreen.GetComponent<Canvas>().enabled = false;
     }
 
     // Enable the canvas and play the video of livingRoom
@@ -58,19 +80,25 @@ public class TransitionRoom : MonoBehaviour
     /// Make the transition of cantina to living room
     /// When a button is trigger
     /// </summary>
-    public void CantinaToLivingRoom()
+    public async void CantinaToLivingRoom()
     {
+        ScreenFade();
+        await Task.Delay(2000);
         disableCantina();
         enableLivingRoom();
+        ScreenUnfade();
     }
 
     /// <summary>
     /// Make the transition of living room to cantina
     /// When a button is trigger
     /// </summary>
-    public void LivingRoomToCantina()
+    public async void LivingRoomToCantina()
     {
+        ScreenFade();
+        await Task.Delay(2000);
         disableLivingRoom();
         enableCantina();
+        ScreenUnfade();
     }
 }
